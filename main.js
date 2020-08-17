2d88f7203f0b14d385a7720a9bd524dcae4ce8cd
Lucia <2313970630@qq.com>
2020-08-18 00:04:38 +0800

    实现ajax

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
-myButton.addEventListener('click',function(e){
+window.jQuery = function() {}
+window.jQuery.ajax = function(options) {
+    let url
+    if (arguments.length === 1) {
+        url = options.url
+    } else if (arguments.length === 2) {
+        url = options[0].url
+        options = options[1]
+    }
+    let method = options.method
+    let body = options.body
+    let successFn = options.successFn
+    let failFn = options.failFn
+    let headers = options.headers
     var request = new XMLHttpRequest()
-    request.onreadystatechange = function(){
-        if(request.readyState === 4){
-            if(request.status >=200 && request.status < 300){
-                let string = request.responseText
-                let object = window.JSON.parse(string)
-                console.log(object.note)
-            }else if(request.readyState >= 400){
-                console.log('fail')
+    request.open(method, url)
+    for (let key in headers) {
+        let value = headers[key]
+        request.setRequestHeader(key, value)
+    }
+    request.onreadystatechange = function() {
+        if (request.readyState === 4) {
+            if (request.status >= 200 && request.status < 300) {
+                successFn.call(undefined, request.responseText)
+            } else if (request.readyState >= 400) {
+                failFn.call(undefined, request)
             }
         }
-     
+
     }
-    request.open('GET','/xxx')
-    request.send()
+    request.send(body)
+}
+window.$ = window.jQuery
+myButton.addEventListener('click', function(e) {
+    window.jQuery.ajax({
+        method: 'POST',
+        url: '/xxx',
+        body: 'a=1&&b=2',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'frank': '18'
+        },
+        successFn: (responseText) => { console.log(1) },
+        failFn: (request) => { console.log(2) }
+    })
 })
